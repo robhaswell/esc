c458f6a7b6715f7adf0d9d7060b446571caa2805
Rob Haswell <me@robhaswell.co.uk>
2016-03-06 14:00:29 +0000

    All parser tests written and passing.

--- parser_test.go
+++ parser_test.go
@@ -28,9 +28,8 @@ func TestNotValidElements(t *testing.T) {
 }
 
 func TestNotValidDoubleCounts(t *testing.T) {
-    cart, err := parseShoppingList("1, 2\n3, 4")
+    _, err := parseShoppingList("1, 2\n3, 4")
     if err == nil {
-        fmt.Printf("Got:\t%v", cart)
         t.Fail()
     }
 }
@@ -52,3 +51,58 @@ func TestCSVItemFirst(t *testing.T) {
         t.Fail()
     }
 }
+
+func TestCSVCountFirst(t *testing.T) {
+    cart, _ := parseShoppingList("100, An item\n200, More items")
+    expected := []CartLine{
+        CartLine{
+            Item:   "An item",
+            Count:  100,
+        },
+        CartLine{
+            Item:   "More items",
+            Count:  200,
+        },
+    }
+    if !reflect.DeepEqual(cart, expected) {
+        fmt.Printf("Got:\t\t%v\nExpected:\t%v\n", cart, expected)
+        t.Fail()
+    }
+}
+
+func TestDTVItemFirst(t *testing.T) {
+    cart, _ := parseShoppingList("An item\t100\nMore items\t200")
+    expected := []CartLine{
+        CartLine{
+            Item:   "An item",
+            Count:  100,
+        },
+        CartLine{
+            Item:   "More items",
+            Count:  200,
+        },
+    }
+    if !reflect.DeepEqual(cart, expected) {
+        fmt.Printf("Got:\t\t%v\nExpected:\t%v\n", cart, expected)
+        t.Fail()
+    }
+}
+
+func TestDTVCountFirst(t *testing.T) {
+    cart, _ := parseShoppingList("100\tAn item\n200\tMore items")
+    expected := []CartLine{
+        CartLine{
+            Item:   "An item",
+            Count:  100,
+        },
+        CartLine{
+            Item:   "More items",
+            Count:  200,
+        },
+    }
+    if !reflect.DeepEqual(cart, expected) {
+        fmt.Printf("Got:\t\t%v\nExpected:\t%v\n", cart, expected)
+        t.Fail()
+    }
+}
+
